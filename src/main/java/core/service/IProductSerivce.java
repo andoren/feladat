41f98f943257c1633e0e7249a1f3953ea617a32a
andoren <mpekar55@gmail.com>
2020-07-27 18:37:58 +0200

    IProductService elkészítve.

---
+++ src/main/java/core/service/IProductSerivce.java
@@ -0,0 +1,16 @@
+package core.service;
+
+import core.model.*;
+
+import java.util.Collection;
+
+public interface IProductSerivce {
+    Collection<Product> getProducts();
+    Collection<Product> getProductsByUserId(User user);
+    Collection<Product> getNotAuthorizedProducts();
+    Collection<Product> getAuthorizedProducts();
+    Product getProductById(int id);
+    Product addProduct(Product newProduct, int userId);
+    boolean modifyProduct(Product product);
+    boolean deleteProductById(int id);
+}
