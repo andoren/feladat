f3e9d9d18223a5e54b2b3ee7b62b42efe457bd44
andoren <mpekar55@gmail.com>
2020-07-27 18:38:53 +0200

    IUserService elkészítve.

---
+++ src/main/java/core/service/IUserService.java
@@ -0,0 +1,16 @@
+package core.service;
+
+import core.model.Role;
+import core.model.User;
+
+import java.util.Collection;
+
+public interface IUserService {
+    Collection<User> getAllUsers();
+    Collection<User> getUsersByRole(Role role);
+    boolean modifyUser(User user);
+    User getUserById(int id);
+    User addUser(User user);
+    boolean deleteUserById(int id);
+    boolean logIn(String username,String password);
+}
