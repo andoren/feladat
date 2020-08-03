80949d321a36f11652822bb7b301d239ee7ae307
misi <mpekar55@gmail.com>
2020-08-03 15:26:05 +0200

    token levétele próba a buyproductbol

--- src/main/java/controller/ProtectedEndpoints.java
+++ src/main/java/controller/ProtectedEndpoints.java
@@ -5,12 +5,15 @@ import core.model.User;
 import core.service.IProductSerivce;
 import core.service.IUserService;
 import helper.JWTHelper;
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.wildfly.swarm.spi.runtime.annotations.Post;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
 @Path("/protected")
@@ -48,5 +51,14 @@ public class ProtectedEndpoints {
     @Produces("application/json")
     public Response getUsers(){
         return Response.ok(userService.getAllUsers()).build();
+    }
+    @Post
+    @Path("/buyproduct")
+    @Produces("application/json")
+    public Response buyProduct(@HeaderParam("Authorization") String token, int productId, int buyerId){
+        System.out.println("Raw token: "+token);
+        String validToken = token.split(":")[1];
+        System.out.println("Valid token: "+validToken);
+        return Response.ok().build();
     }
 }
